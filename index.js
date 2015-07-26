e0bc7c2f2c3880aafbfb57c7a77822bc17c35f4f
Jake Russo <madcap.russo@gmail.com>
2015-07-26 13:00:09 -0500

    Add started flag and move resolve into event :art:

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
 const nodemon = require("nodemon")
 
 var server
+var started = false
 
 module.exports = function () {
   this.nodemon = function (opts) {
     return new Promise((resolve, reject) => {
-      if (!server) {
+      if (started) { server.emit("restart") }
+      else {
         server = nodemon(opts)
           .on("start", function () {
             this.log("nodemon started.")
+            resolve()
           }.bind(this))
           .on("crash", function () {
             this.error("script crashed for some reason")
             reject()
           }.bind(this))
-      } else {
-        server.emit("restart")
       }
-      resolve()
     })
   }
 }
