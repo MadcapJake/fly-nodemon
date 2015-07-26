bf6c84a7bb824aa89e176f1f2cd5fe7f056b4ca3
Jake Russo <madcap.russo@gmail.com>
2015-07-26 12:21:59 -0500

    Add conditional start/restart logic :construction:

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 const nodemon = require("nodemon")
+
+var server = undefined
+
 module.exports = function () {
   this.nodemon = function (opts) {
-    
+    if (!server) {
+      server = nodemon(opts)
+        .on("start", function () {
+        this.log("nodemon started.")
+        })
+        .on("crash", function () {
+          this.error("script crashed for some reason")
+        })
+    } else {
+      server.emit("restart")
+    }
   }
 }
